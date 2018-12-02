a27b62a101f9f806cdac615e8968e401bc4b7686
ujjwalshrestha1996 <ujjwalshrestha1996@gmail.com>
2018-12-02 14:31:45 +0545

    version control system

--- c7181420 shuva/Bug Tracker/BugTrackingSoftware/BugTrackingSoftware/LoginFrm.Designer.cs
+++ c7181420 shuva/Bug Tracker/BugTrackingSoftware/BugTrackingSoftware/LoginFrm.Designer.cs
@@ -35,6 +35,7 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.btnLogin = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -97,11 +98,22 @@
             this.btnLogin.UseVisualStyleBackColor = true;
             this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(267, 235);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 30);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "github";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // LoginFrm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(426, 284);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.groupBox1);
             this.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -124,5 +136,6 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button button1;
     }
 }
--- c7181420 shuva/Bug Tracker/BugTrackingSoftware/BugTrackingSoftware/LoginFrm.cs
+++ c7181420 shuva/Bug Tracker/BugTrackingSoftware/BugTrackingSoftware/LoginFrm.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using DataAccessLayer;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 namespace BugTrackingSoftware
 {
     public partial class LoginFrm : Form
@@ -58,6 +60,24 @@ namespace BugTrackingSoftware
 
         private void txtUserName_TextChanged(object sender, EventArgs e)
         {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IWebDriver driver = new ChromeDriver();
+            // Launch the ToolsQA WebSite
+            driver.Url = ("https://github.com/shuvakc/bugtrace");
+
+            // Type Name in the FirstName text box      
+            //driver.FindElement(By.Name("firstname")).SendKeys("shuvakc");
+            driver.FindElement(By.Id("user_login")).SendKeys("shuvaprabhatkc2359@gmail.com");
+
+            //Type LastName in the LastName text box
+            driver.FindElement(By.Id("user_password")).SendKeys("shuvakc2359");
+
+            // Click on the Submit button
+            driver.FindElement(By.Name("commit")).Click();
 
         }
     }
